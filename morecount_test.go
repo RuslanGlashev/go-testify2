fc7fe6eb42393f7592933c6cbb656e36efcc6b63
Gerbicid <imyslivtsev@.com>
2024-12-16 18:19:33 +0300

    first-commit

---
+++ morecount_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	assert.NotEmpty(t, body)
+	assert.LessOrEqual(t, len(list), totalCount)
+}
